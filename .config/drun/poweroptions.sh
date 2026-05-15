da20a635cdc4cc9ae9ea186a7eda78fb72a93cda
Mick <51725512+MickSedlack@users.noreply.github.com>
2026-05-15 03:11:31 -0400

    Update poweroptions.sh
    Corrected for i3

--- .config/drun/poweroptions.sh
+++ .config/drun/poweroptions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 entries=('Reload' 'Logout' 'Suspend' 'Reboot' 'Shutdown')
 
-selected=$(printf '%s\n' ${entries[@]} | dmenu -i -p 'Power options' -fn 'Cozettevector-16')
+selected=$(printf '%s\n' ${entries[@]} | dmenu -i -p 'Power options' -fn 'Cozettevector-16' -l 4)
 
 case $selected in
 	Reload) _pkill sxhkd && sxhkd ;;
-	Logout) _pkill cwm ;;
+	Logout) _pkill -u $USER ;;
 	Suspend) systemctl suspend ;;
 	Reboot) systemctl reboot ;;
 	Shutdown) systemctl shutdown ;;
