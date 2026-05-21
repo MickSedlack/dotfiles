60e78ac21d4021e7e36f6462bf6c5f056ff842b6
Mick <51725512+MickSedlack@users.noreply.github.com>
2026-05-20 20:52:34 -0400

    Add dunst configs
    Dunst volume control script, and dunstrc.

---
+++ .config/dunst/dun_vol.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+#
+#
+# Takes up, down, and mute as args.
+# This version is pulseaudio/pipewire
+# Alongside pamixer
+#
+# This little script stands on these shoulders:
+# https://github.com/dastorm/volume-notification-dunst/blob/master/volume.sh
+# https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a
+# https://gist.github.com/Blaradox/030f06d165a82583ae817ee954438f2e
+# https://holmdev.github.io/articles/creating-a-volume-display-script/
+
+case $1 in
+  up)
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
+    dunstify "Volume: $(printf "%03d%%" $(pamixer --get-volume))" \
+	    -i audio-volume-high \
+	    -r 22328 \
+	    -h int:value:$(pamixer --get-volume)
+    ;;
+  down)
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+    dunstify  "Volume: $(printf "%03d%%" $(pamixer --get-volume))" \
+	    -i audio-volume-low \
+	    -r 22328 \
+	    -h int:value:$(pamixer --get-volume)
+    ;;
+  mute)
+    # toggle mute
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+    dunstify -i audio-volume-muted -r 22328 -u normal "mute"
+    ;;
+esac
