eea4eb4300a8e9606625cbd98cc1abb31bbff0a5
Mick <51725512+MickSedlack@users.noreply.github.com>
2026-06-17 22:43:02 +0000

    Update customrun.sh
    I3-dmenu and dmenu-run are options.

--- .config/drun/customrun.sh
+++ .config/drun/customrun.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
 
-dmenu_run -i -p '♥' -fn 'Inconsolata Nerd Font Mono-16'
+case $1 in
+	1) 
+	i3-dmenu-desktop --dmenu "dmenu -i -p '♥' -fn 'Inconsolata Nerd Font Mono-16'";;
+	2)
+	dmenu_run -i -p '♥' -fn 'Inconsolata Nerd Font Mono-16';;
+esac`
