2aa8a71d0c026239a38ca09d80896d841a7c4755
Mick <51725512+MickSedlack@users.noreply.github.com>
2026-08-25 15:10:13 -0400

    Update poweroptions.sh

--- .config/drun/poweroptions.sh
+++ .config/drun/poweroptions.sh
@@ -4,8 +4,8 @@ entries=('Reload' 'Logout' 'Suspend' 'Reboot' 'Shutdown')
 selected=$(printf '%s\n' ${entries[@]} | dmenu -i -p 'Power options' -fn 'Inconsolata Nerd Font Mono-16')
 
 case $selected in
-	Reload) _pkill sxhkd && sxhkd ;;
-	Logout) _pkill -u $USER ;;
+	Reload) pkill sxhkd && sxhkd ;;
+	Logout) pkill -u $USER ;;
 	Suspend) systemctl suspend ;;
 	Reboot) systemctl reboot ;;
 	Shutdown) systemctl shutdown ;;
